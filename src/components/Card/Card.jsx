e7bcb05178783b6ab75d0a77c204f0323b7972b7
João Victor Dorp <jvictordorp@gmail.com>
2024-10-04 15:09:27 -0300

    melhor na exibição

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import "./Card.css";
 
 export default function Card(props) {
-    function formatarMatriz(matriz) {
-        return matriz.map(row => row.join(" ")).join("\n");
+    function separarLinhasMatriz(matrizString) {
+        // Remove os colchetes externos e espaços extras
+        const matrizSemColchetes = matrizString.trim().slice(1, -1);
+    
+        // Divide a string nas vírgulas entre os colchetes
+        const linhas = matrizSemColchetes.match(/\[.*?\]/g);
+    
+        // Retorna as linhas como um vetor de strings
+        return linhas;
     }
+    
+    const resultado = separarLinhasMatriz(props.result);
+    console.log(resultado);
+    
 
     return (
         <div className="CardField" style={{ maxHeight: "100%" }}>
             <div className="Title">{props.title}</div>
-            <div className="Content"><pre>{formatarMatriz(props.result)}</pre></div>
+            <div className="Content">{resultado[0]}</div>
+            <div className="Content">{resultado[1]}</div>
+            <div className="Content">{resultado[2]}</div>
+            <div className="Content">{resultado[3]}</div>
         </div>
     );
 }
