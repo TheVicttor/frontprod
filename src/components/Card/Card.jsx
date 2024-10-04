b56c7a55e2eaba421b0e5f5e4927101fc27b2b2e
João Victor Dorp <jvictordorp@gmail.com>
2024-10-04 14:59:06 -0300

    formatação matriz

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./Card.css";
 
 export default function Card(props) {
+    function formatarMatriz(matriz) {
+        return matriz.map(row => row.join(" ")).join("\n");
+    }
+
     return (
         <div className="CardField" style={{ maxHeight: "100%" }}>
             <div className="Title">{props.title}</div>
-            <div className="Content">{props.result}</div>
+            <div className="Content"><pre>{formatarMatriz(props.result)}</pre></div>
         </div>
     );
 }
