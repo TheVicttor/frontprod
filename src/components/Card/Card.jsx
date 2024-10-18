dd35569899ef4b8416215041e607cba8ab7ef073
João Victor Dorp <jvictordorp@gmail.com>
2024-10-17 23:40:26 -0300

    fix bug ricciScalar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,17 @@ import "./Card.css";
 export default function Card(props) {
     function separarLinhasMatriz(matrizString) {
         // Remove os colchetes externos e espaços extras
-        const matrizSemColchetes = matrizString.trim().slice(1, -1);
-    
-        // Divide a string nas vírgulas entre os colchetes
-        const linhas = matrizSemColchetes.match(/\[.*?\]/g);
-    
+        if (matrizString.startsWith('[')) {
+
+            const matrizSemColchetes = matrizString.trim().slice(1, -1);
+            
+            // Divide a string nas vírgulas entre os colchetes
+            const linhas = matrizSemColchetes.match(/\[.*?\]/g);
+        }
+        else { 
+            linhas = [matrizString]
+        }
+        
         // Retorna as linhas como um vetor de strings
         return linhas;
     }
